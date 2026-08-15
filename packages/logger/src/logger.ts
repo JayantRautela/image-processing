3258924aec0608b385ba02f0aaf8add871dceb43
JayantRautela <jayantrautela0205@gmail.com>
2026-08-15 22:35:53 +0530

    feat(package): add logger package

---
+++ packages/logger/src/logger.ts
@@ -0,0 +1,23 @@
+import pino from "pino";
+import pinoHttp from "pino-http";
+
+const isDevelopment = process.env.NODE_ENV !== "production";
+
+export const logger = pino({
+  level: process.env.LOG_LEVEL || "info",
+
+  ...(isDevelopment && {
+    transport: {
+      target: "pino-pretty",
+      options: {
+        colorize: true,
+        translateTime: "SYS:standard",
+        ignore: "pid,hostname"
+      }
+    }
+  })
+});
+
+export const httpLogger = pinoHttp({
+  logger
+});
