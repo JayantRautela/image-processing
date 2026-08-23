443b8cc62b50e7615e2531ce898c76ef53952d7d
JayantRautela <jayantrautela0205@gmail.com>
2026-08-23 22:40:08 +0530

    feat(package): add zod package

---
+++ packages/zod/src/schema.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+
+export const enterUserSchema = z.object({
+  email: z.email(),
+});
+
+export const enterUserOtpSchema = z.object({
+  email: z.email(),
+  otp: z.string().length(6, { message: "OTP length must be 6"}),
+});
